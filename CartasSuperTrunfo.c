0929198286c13c241c32231afd19a8f2188484be
bicutoru <106106717+victorashino@users.noreply.github.com>
2026-02-04 10:24:18 -0300

    Atualizaçõ para o nível Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
 
 int main() {
-  char state, code[5], city[50], state2, code2[5], city2[50];
-  int population, atractions, population2, atractions2;
-  float area, gdp, area2, gdp2;
-
+  char first_card_state, first_card_code[5], first_card_city[50];
+  char second_card_state, second_card_code[5], second_card_city[50];
+  int first_card_population, first_card_atractions;
+  int second_card_population, second_card_atractions;
+  float first_card_area, first_card_gdp, first_card_population_density, first_card_gdp_percapita;
+  float second_card_area, second_card_gdp, second_card_population_density, second_card_gdp_percapita;
 
   // First card input
   printf("CARTA 1\n");
 
   printf("Insira o estado(letra A~H): ");
-  scanf("%c", &state);
+  scanf("%c", &first_card_state);
 
   printf("Insira o número da carta: ");
-  scanf("%s", code);
+  scanf("%s", first_card_code);
 
   printf("Insira o nome da cidade: ");
-  scanf("%s", city);
+  scanf("%s", first_card_city);
 
   printf("População: ");
-  scanf("%d", &population);
+  scanf("%d", &first_card_population);
 
   printf("Área em Km^2: ");
-  scanf("%f", &area);
+  scanf("%f", &first_card_area);
+  first_card_population_density = first_card_area / first_card_population;
 
   printf("PIB: ");
-  scanf("%f", &gdp);
+  scanf("%f", &first_card_gdp);
+  first_card_gdp_percapita = first_card_gdp / first_card_population;
 
   printf("Número de pontos turísticos: ");
-  scanf("%d", &atractions);
+  scanf("%d", &first_card_atractions);
 
 
 
@@ -37,47 +41,53 @@ int main() {
   printf("CARTA 2\n");
 
   printf("Insira o estado(letra A~H): ");
-  scanf(" %c", &state2);
+  scanf(" %c", &second_card_state);
 
   printf("Insira o número da carta: ");
-  scanf("%s", code);
+  scanf("%s", first_card_code);
 
   printf("Insira o nome da cidade: ");
-  scanf("%s", city2);
+  scanf("%s", second_card_city);
 
   printf("População: ");
-  scanf("%d", &population);
+  scanf("%d", &first_card_population);
 
   printf("Área em Km^2: ");
-  scanf("%f", &area2);
+  scanf("%f", &second_card_area);
+  second_card_population_density = second_card_area / second_card_population;
 
   printf("PIB: ");
-  scanf("%f", &gdp2);
+  scanf("%f", &second_card_gdp);
+  second_card_gdp_percapita = second_card_gdp / second_card_population;
 
   printf("Número de pontos turísticos: ");
-  scanf("%d", &atractions2);
+  scanf("%d", &second_card_atractions);
 
   // First card output
   printf("\n\n");
   printf("CARTA 1:\n");
-  printf("Estado: %c\n", state);
-  printf("Código: %c%s\n", state, code);
-  printf("Nome da cidade: %s\n", city);
-  printf("População: %d\n", population);
-  printf("Área: %.2f Km^2\n", area);
-  printf("PIB: %.2f bilhoẽs de reais\n", gdp);
-  printf("Número de pontos turísticos: %d\n", atractions);
+  printf("Estado: %c\n", first_card_state);
+  printf("Código: %c%s\n", first_card_state, first_card_code);
+  printf("Nome da cidade: %s\n", first_card_city);
+  printf("População: %d\n", first_card_population);
+  printf("Área: %.2f Km^2\n", first_card_area);
+  printf("PIB: %.2f bilhoẽs de reais\n", first_card_gdp);
+  printf("Número de pontos turísticos: %d\n", first_card_atractions);
+  printf("O PIB per capita é: %.2f\n", first_card_gdp_percapita);
+  printf("A Densidade populacional é de: %.2f", first_card_population_density);
 
   // Second card output
   printf("\n\n");
   printf("CARTA 2:\n");
-  printf("Estado: %c\n", state2);
-  printf("Código: %c%s\n", state2, code2);
-  printf("Nome da cidade: %s\n", city2);
-  printf("População: %d\n", population2);
-  printf("Área: %.2f Km^2\n", area2);
-  printf("PIB: %.2f bilhoẽs de reais\n", gdp2);
-  printf("Número de pontos turísticos: %d\n", atractions2);
+  printf("Estado: %c\n", second_card_state);
+  printf("Código: %c%s\n", second_card_state, second_card_code);
+  printf("Nome da cidade: %s\n", second_card_city);
+  printf("População: %d\n", second_card_population);
+  printf("Área: %.2f Km^2\n", second_card_area);
+  printf("PIB: %.2f bilhoẽs de reais\n", second_card_gdp);
+  printf("Número de pontos turísticos: %d\n", second_card_atractions);
+  printf("O PIB per capita é: %.2f\n", second_card_gdp_percapita);
+  printf("A Densidade populacional é de: %.2f", second_card_population_density);
   
   return 0;
 }
